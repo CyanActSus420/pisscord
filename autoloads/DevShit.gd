8aceb4eadccb27cb3e11d7932fef5663f6c0d1c8
CyanActSus420 <cyanactsus@gmail.com>
2025-02-10 21:46:35 +0100

    removed DevShit.gd because

--- autoloads/DevShit.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-# all this shit is for the dev stuff
-# mostly cosmetic shit to show that im cooler than others because i can do cool shit :money_mouth:
--- autoloads/LocalUserData.gd
+++ autoloads/LocalUserData.gd
@@ -5,7 +5,8 @@ var color:String = "white"
 var serverIP:String = "localhost"
 var serverPort:int = 42069
 var guest = false
+var connected = false
+
+# i should probably rewrite this now that i made the thing open source :cold_face:
 var host = false
 var dedicated_server = false
-
-var connected = false
