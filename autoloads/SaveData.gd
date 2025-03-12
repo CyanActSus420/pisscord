eed29c8dcb7c9e74332bcb348fdf3ded5915532a
CyanActSus420 <cyanactsus@gmail.com>
2025-03-12 21:22:50 +0100

    theme management

--- autoloads/SaveData.gd
+++ autoloads/SaveData.gd
@@ -1,13 +1,22 @@
 extends Node
 
 var config = ConfigFile.new()
+var theme_manager_file = ConfigFile.new()
 var user_theme = ConfigFile.new()
 
+var usedTheme = "base_theme"
+
 var save_path = "user://fav_servers.cfg"
-var theme_path = "user://user_theme.cfg"
+var theme_manager = "user://theme-manager.cum"
+var base_theme_path = "user://base_theme.cbt"
+
+var cur_path = "user://base_theme.cbt"
 
 func _ready() -> void:
-	create_custom_theme()
+	create_base_theme()
+	create_theme_manager()
+	
+	set_path()
 
 func save_to_config(name_uwu: String, var_to_save):
 	config.set_value("file", name_uwu, var_to_save)
@@ -17,16 +26,24 @@ func load_from_config(name_uwu: String):
 	var file = config.load(save_path)
 	
 	if file != OK:
-		push_warning("user://fav_servers.cfg doesnt exist or wasnt loaded properly")
+		push_error("user://fav_servers.cfg doesnt exist or wasnt loaded properly")
 		return
 	
 	return config.get_value("file", name_uwu)
 
-func create_custom_theme():
-	var file = user_theme.load(theme_path)
+func create_theme_manager():
+	var file = theme_manager_file.load(theme_manager)
 	
 	if file != OK:
-		push_warning("creating new user://user_theme.cfg file")
+		theme_manager_file.set_value("theme-management", "user_theme_file_name", "base_theme")
+		
+		theme_manager_file.save(theme_manager)
+
+func create_base_theme():
+	var file = user_theme.load(base_theme_path)
+	
+	if file != OK:
+		print("creating new user://base_theme.cfg file")
 		# too lazy to make the code not shit
 		user_theme.set_value("colors", "bg_color", ThemeManager.default_bg_color)
 		user_theme.set_value("chat-menu", "chat_size", ThemeManager.default_chat_size)
@@ -44,13 +61,23 @@ func create_custom_theme():
 		user_theme.set_value("chat-menu", "info_position", ThemeManager.default_info_position)
 		user_theme.set_value("chat-menu", "usrcol_position", ThemeManager.default_usrcol_position)
 		
-		user_theme.save(theme_path)
+		user_theme.save(base_theme_path)
+
+func set_path():
+	var file = theme_manager_file.load(theme_manager)
+	
+	var themeName = theme_manager_file.get_value("theme-management", "user_theme_file_name")
+	
+	cur_path = "user://%s.cbt" % themeName
 
 func load_custom_theme_property(shit:String, property_name:String):
-	var file = user_theme.load(theme_path)
+	var file = user_theme.load(cur_path)
 	
 	if file != OK:
-		push_warning("user://user_theme.cfg doesnt exist or wasnt loaded properly")
+		push_error("user://user_theme.cfg doesnt exist or wasnt loaded properly")
 		return
 	
-	return user_theme.get_value(shit, property_name)
+	var value = user_theme.get_value(shit, property_name)
+	
+	print(value)
+	return value
--- autoloads/ThemeManager.gd
+++ autoloads/ThemeManager.gd
@@ -18,6 +18,7 @@ var default_usrcol_position = Vector2(484, -308)
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("reset_theme"):
+		SaveData.set_path()
 		reload_theme()
 
 func reload_theme():
