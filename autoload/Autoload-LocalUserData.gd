64aacde1ed431309f7ecb342efd3b3150545f6aa
CyanActSus420 <cyanactsus@gmail.com>
2026-02-10 22:10:51 +0100

    removed pointless @exports

--- autoload/Autoload-LocalUserData.gd
+++ autoload/Autoload-LocalUserData.gd
@@ -3,4 +3,4 @@ extends Node
 var peer:ENetMultiplayerPeer = null
 var client = null
 
-@export var Username:String = "ass"
+var Username:String = "ass"
--- src/resources/Resource-MessageData.gd
+++ src/resources/Resource-MessageData.gd
@@ -1,8 +1,8 @@
 class_name MessageData extends Resource
 
-@export var MessageContent:String = ""
-@export var Username:String = ""
-@export var UserColor:String = "[color=white]"
+var MessageContent:String = ""
+var Username:String = ""
+var UserColor:String = "[color=white]"
 
 func return_formatted() -> String:
 	return "%s: %s" % [
